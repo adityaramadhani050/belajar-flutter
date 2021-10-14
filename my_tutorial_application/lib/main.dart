5a02d45e1c466dc8fe6ec94632f361e97e5fd6bb
adityaramadhani050 <adityaramadhani050@gmail.com>
2021-10-14 15:10:20 +0700

    Row, Column and Container Decoration

--- my_tutorial_application/lib/main.dart
+++ my_tutorial_application/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -12,27 +13,122 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Hello World!'),
+          title: const Text('Latihan Row dan Column'),
         ),
-        body: Center(
-          child: Container(
-            width: 200,
-            height: 80,
-            color: Colors.blue,
-            padding: const EdgeInsets.all(10),
-            child: const Center(
-              child: Text(
-                'Welcome to My First App. this app is made using Flutter framework',
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    color: Colors.white,
-                    fontWeight: FontWeight.bold,
-                    fontSize: 16),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Container(
+              height: 250,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.lightBlueAccent,
+              child: Column(
+                children: [
+                  const Padding(
+                    padding: EdgeInsets.only(bottom: 5),
+                    child: Text(
+                      'ALBUM 1',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16,
+                          color: Colors.white),
+                    ),
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceAround,
+                    children: [
+                      Container(
+                        width: 150,
+                        height: 200,
+                        color: Colors.red,
+                        padding: const EdgeInsets.fromLTRB(7, 12, 7, 0),
+                        child: Center(
+                          child: Column(
+                            children: [
+                              Container(
+                                width: double.infinity,
+                                height: 150,
+                                margin: const EdgeInsets.only(bottom: 5),
+                                decoration: BoxDecoration(
+                                  borderRadius: BorderRadius.circular(10),
+                                  gradient: const LinearGradient(
+                                    begin: Alignment.topLeft,
+                                    end: Alignment.bottomRight,
+                                    colors: [Colors.yellow, Colors.blue],
+                                  ),
+                                ),
+                              ),
+                              const Text(
+                                'Foto 1',
+                                style: TextStyle(
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 16,
+                                    color: Colors.white),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Container(
+                        width: 150,
+                        height: 200,
+                        color: Colors.orange,
+                        child: const Center(
+                          child: Text(
+                            'Kotak 2',
+                            style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                fontSize: 16,
+                                color: Colors.white),
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                ],
               ),
             ),
-          ),
+            Container(
+              height: 250,
+              margin: const EdgeInsets.all(10),
+              padding: const EdgeInsets.all(10),
+              color: Colors.limeAccent,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  Container(
+                    width: 150,
+                    height: 100,
+                    color: Colors.green,
+                    child: const Center(
+                      child: Text(
+                        'Kotak 3',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    width: 150,
+                    height: 100,
+                    color: Colors.blue,
+                    child: const Center(
+                      child: Text(
+                        'Kotak 4',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 16,
+                            color: Colors.white),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
         ),
       ),
     );
