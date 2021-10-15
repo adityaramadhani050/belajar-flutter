9f17379180f2654a8435cc50bad5ad26351b1e69
adityaramadhani050 <adityaramadhani050@gmail.com>
2021-10-15 20:15:46 +0700

    button modification and counter text color based number

--- my_tutorial_application/lib/main.dart
+++ my_tutorial_application/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,8 +28,13 @@ class _MyAppState extends State<MyApp> {
               Text(
                 counter.toString(),
                 style: TextStyle(
+                  color: (counter == 10)
+                      ? Colors.green
+                      : (counter == 0)
+                          ? Colors.red
+                          : Colors.blue,
                   fontWeight: FontWeight.bold,
-                  fontSize: 30.0 + counter,
+                  fontSize: 30.0 + (counter * 2),
                 ),
               ),
               const SizedBox(
@@ -38,20 +44,48 @@ class _MyAppState extends State<MyApp> {
                 mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
                   ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
+                      fixedSize: MaterialStateProperty.all(const Size(120, 30)),
+                      elevation: MaterialStateProperty.all(15.0),
+                      shadowColor: MaterialStateProperty.all(Colors.green),
+                    ),
                     onPressed: () {
                       setState(() {
-                        counter++;
+                        if (counter < 10) {
+                          counter++;
+                        }
                       });
                     },
-                    child: const Text('+'),
+                    child: const Text(
+                      '+',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                   ElevatedButton(
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
+                      fixedSize: MaterialStateProperty.all(const Size(120, 30)),
+                      elevation: MaterialStateProperty.all(15.0),
+                      shadowColor: MaterialStateProperty.all(Colors.red),
+                    ),
                     onPressed: () {
                       setState(() {
-                        counter--;
+                        if (counter > 0) {
+                          counter--;
+                        }
                       });
                     },
-                    child: const Text('-'),
+                    child: const Text(
+                      '-',
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
                   ),
                 ],
               )
