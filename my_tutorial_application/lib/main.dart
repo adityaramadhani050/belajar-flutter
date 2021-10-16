06909879b290c71f95f3984d62b5f55a1e388fbe
adityaramadhani050 <adityaramadhani050@gmail.com>
2021-10-16 12:00:37 +0700

    List & List View

--- my_tutorial_application/lib/main.dart
+++ my_tutorial_application/lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/painting.dart';
 
 void main() {
   runApp(const MyApp());
@@ -20,77 +19,44 @@ class _MyAppState extends State<MyApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Stateless Stateful Widget')),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                counter.toString(),
-                style: TextStyle(
-                  color: (counter == 10)
-                      ? Colors.green
-                      : (counter == 0)
-                          ? Colors.red
-                          : Colors.blue,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 30.0 + (counter * 2),
+        appBar: AppBar(title: const Text('List & ListView')),
+        body: Column(
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      counter++;
+                    });
+                  },
+                  child: const Text('Tambah'),
                 ),
+                ElevatedButton(
+                  onPressed: () {
+                    setState(() {
+                      if (counter > 0) {
+                        counter--;
+                      }
+                    });
+                  },
+                  child: const Text('Kurang'),
+                ),
+              ],
+            ),
+            Expanded(
+              child: ListView.builder(
+                itemCount: counter,
+                itemBuilder: (context, index) {
+                  return Text(
+                    'Data ke-$index',
+                    style: const TextStyle(fontSize: 25),
+                  );
+                },
               ),
-              const SizedBox(
-                height: 50,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceAround,
-                children: [
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.green),
-                      fixedSize: MaterialStateProperty.all(const Size(120, 30)),
-                      elevation: MaterialStateProperty.all(15.0),
-                      shadowColor: MaterialStateProperty.all(Colors.green),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        if (counter < 10) {
-                          counter++;
-                        }
-                      });
-                    },
-                    child: const Text(
-                      '+',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                  ElevatedButton(
-                    style: ButtonStyle(
-                      backgroundColor: MaterialStateProperty.all(Colors.red),
-                      fixedSize: MaterialStateProperty.all(const Size(120, 30)),
-                      elevation: MaterialStateProperty.all(15.0),
-                      shadowColor: MaterialStateProperty.all(Colors.red),
-                    ),
-                    onPressed: () {
-                      setState(() {
-                        if (counter > 0) {
-                          counter--;
-                        }
-                      });
-                    },
-                    child: const Text(
-                      '-',
-                      style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                      ),
-                    ),
-                  ),
-                ],
-              )
-            ],
-          ),
+            ),
+          ],
         ),
       ),
     );
