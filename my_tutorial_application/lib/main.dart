215a8d464217ab1c36cdc7948f35a9d337aa030b
adityaramadhani050 <adityaramadhani050@gmail.com>
2021-10-18 01:54:39 +0700

    Stack and Align Widget

--- my_tutorial_application/lib/main.dart
+++ my_tutorial_application/lib/main.dart
@@ -1,60 +1,112 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  @override
+  State<MyApp> createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int _counter = 0;
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-        appBar: AppBar(title: const Text('Flexible Layout')),
-        body: Column(
+        appBar: AppBar(title: const Text('Stack & Align Widget')),
+        body: Stack(
           children: [
-            Flexible(
-              flex: 1,
+            Column(
+              children: [
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.blueGrey,
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Flexible(
+                  flex: 1,
+                  child: Row(
+                    children: [
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.blueGrey,
+                        ),
+                      ),
+                      Flexible(
+                        flex: 1,
+                        child: Container(
+                          color: Colors.grey,
+                        ),
+                      )
+                    ],
+                  ),
+                )
+              ],
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: ListView.builder(
+                itemCount: _counter,
+                itemBuilder: (_, index) => Text(
+                  'Ini adalah teks ke-${index + 1}',
+                  style: const TextStyle(
+                    fontSize: 30,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: const Alignment(0, 0.8),
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
                 children: [
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.red,
-                      margin: const EdgeInsets.all(5),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        _counter++;
+                      });
+                    },
+                    child: const Text('Tambah'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.green),
                     ),
                   ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.yellow,
-                      margin: const EdgeInsets.all(5),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    child: Container(
-                      color: Colors.orange,
-                      margin: const EdgeInsets.all(5),
+                  ElevatedButton(
+                    onPressed: () {
+                      setState(() {
+                        if (_counter > 0) {
+                          _counter--;
+                        }
+                      });
+                    },
+                    child: const Text('Kurang'),
+                    style: ButtonStyle(
+                      backgroundColor: MaterialStateProperty.all(Colors.red),
                     ),
                   ),
                 ],
-              ),
-            ),
-            Flexible(
-              flex: 2,
-              child: Container(
-                color: Colors.green,
-                margin: const EdgeInsets.all(5),
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              child: Container(
-                color: Colors.blue,
-                margin: const EdgeInsets.all(5),
               ),
             ),
           ],
